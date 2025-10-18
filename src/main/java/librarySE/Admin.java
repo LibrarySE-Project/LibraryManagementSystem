a2b44d8f8c4be50d832e5b5e4b188ff0ee8dc9ea
Eman <Eman@DESKTOP-VQA33N6>
2025-10-18 21:55:36 +0300

    Admin update

---
+++ src/main/java/librarySE/Admin.java
@@ -0,0 +1,48 @@
+package librarySE;
+
+public class Admin {
+	private static Admin instance;
+	
+    private String username;
+    private String password;
+    private boolean loggedIn;
+
+    private Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.loggedIn = false; 
+    }
+
+    public boolean login(String enteredUser, String enteredPass) {
+        if (username.equals(enteredUser) && password.equals(enteredPass)) {
+            loggedIn = true;
+            return true;
+        } else {
+            loggedIn = false;
+            return false;
+        }
+    }
+    public static Admin getInstance(String username, String password) {
+        if (instance == null) {
+            instance = new Admin(username, password); 
+        }
+        return instance;
+    }
+
+    public void logout() {
+        loggedIn = false;
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+
+
+}
+
